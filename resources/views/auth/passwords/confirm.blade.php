2a55e16b218da2b06ddefa0f632531dc4105d42f
GhassanYusuf <ghassan.yousif.83@gmail.com>
2022-05-29 21:59:23 +0300

    worked with admin let there takeone login

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<body class="hold-transition login-page bg-secondary">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -46,4 +47,5 @@
         </div>
     </div>
 </div>
+</body>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<body class="hold-transition login-page bg-secondary">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -62,4 +63,5 @@
         </div>
     </div>
 </div>
+</body>
 @endsection
